73e086329cd92127b8ffc5b73f2ce9fc4f65c2aa
Radiant-Wizard <hei.taratra@gmail.com>
2025-11-29 11:31:36 +0300

    feat: created the service layer of renter

---
+++ app/services/renterService.py
@@ -0,0 +1,14 @@
+from app.repositories.RenterRepository import RenterRepository
+from app.models.customer import Customer
+from typing import List
+
+
+class RenterService:
+    def __init__(self, repository: RenterRepository):
+        self.repository = repository
+
+    def createRenter(self, renter: Customer) -> int:
+        return self.repository.create(renter)
+
+    def getAllRenters(self) -> List[Customer]:
+        return self.repository.getAll()
