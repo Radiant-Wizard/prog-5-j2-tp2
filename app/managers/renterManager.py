3db438c99f95d1d2353d3a588784a25692ad03b7
Radiant-Wizard <hei.taratra@gmail.com>
2025-11-29 12:08:29 +0300

    fix: fixed the customer instantiation issue

--- app/managers/renterManager.py
+++ app/managers/renterManager.py
@@ -1,5 +1,7 @@
 from app.services.renterService import RenterService
 from app.models.customer import Customer
+from app.models.entreprise import Entreprise
+from app.models.person import Person
 from typing import List
 
 
@@ -8,7 +10,10 @@ class RenterManager:
         self.service = service
 
     def addRenter(self, name: str, type_: str) -> int:
-        renter = Customer(name, [])
+        if str(type_) == "ENTREPRISE":
+            renter = Entreprise(name, [])
+            renter.type = type_
+        renter = Person(name, [])
         renter.type = type_
         return self.service.createRenter(renter)
 
